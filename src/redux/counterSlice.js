bcd5f84234e8f04f3c0b87d13e8eb8053327c096
Jason Lee <jslee423@gmail.com>
2022-10-21 19:03:02 -0400

    remove example reducers

--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -7,19 +7,6 @@ export const counterSlice = createSlice({
     numberOfPages: 0,
   },
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
     setLastNumber: (state, action) => {
         state.lastNumber = action.payload
     },
